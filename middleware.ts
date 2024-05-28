bddca683802a85b1a0fcf754e205a8d62d45b1de
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-05-28 20:51:15 +0530

    implemented clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware(
+//   {
+//     publicRoutes:['/api/webhooks/clerk']
+//   }
+// );
+ 
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes:['/api/webhooks/clerk']
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
-
-// import { authMiddleware } from "@clerk/nextjs/server";
-
-// export default authMiddleware();
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],};
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],};
